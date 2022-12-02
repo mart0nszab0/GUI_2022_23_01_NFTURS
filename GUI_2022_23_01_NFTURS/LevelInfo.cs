5717ef1f68cc99275beecc5dc1482b5ec68ab1d0
martonszabo <martonszabo@stud.uni-obuda.hu>
2022-12-02 14:14:39 +0100

    starting to extract level info into a static class, there's more to do later

---
+++ GUI_2022_23_01_NFTURS/LevelInfo.cs
@@ -0,0 +1,17 @@
+﻿using GUI_2022_23_01_NFTURS.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_2022_23_01_NFTURS
+{
+    public static class LevelInfo
+    {
+        static int NUMBER_OF_LEVELS()
+        {
+            return new GameLogic(1).NUMBER_OF_LEVELS;
+        }
+    }
+}
